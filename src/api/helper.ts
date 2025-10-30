e64cd124b0c8e76099e7434815e418829d3b1cd6
gexgd0419 <55008943+gexgd0419@users.noreply.github.com>
2025-10-30 19:07:21 +0800

    Fix: Filter out comment lines in EventStream

--- src/api/helper.ts
+++ src/api/helper.ts
@@ -6,6 +6,7 @@ export const parseEventSource = (
   const result = data
     .split('\n\n')
     .filter(Boolean)
+    .filter((line) => !line.startsWith(':'))
     .map((chunk) => {
       const jsonString = chunk
         .split('\n')
